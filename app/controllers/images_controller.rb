e5ea26c5245c357ec9d64c96921df2174eb157ee
BLaurenB <blbillington1@gmail.com>
2017-10-23 22:04:05 -0600

    admin can create images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -12,6 +12,22 @@ class ImagesController < ActionController::Base
   end
 
   def new
+    @image = Image.new
+  end
+
+  def create
+    image = Image.new(image_params)
+    if image.save
+      redirect_to images_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:title, :image_path)
   end
 
 end
---
+++ spec/features/images/admin_creates_image_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Admin visits Image index," do
+  scenario "they see h1 and link to create new images" do
+    visit new_image_path
+
+    fill_in 'image[title]', with: "Rainbow"
+    fill_in 'image[image_path]', with: "https://static.pexels.com/photos/108941/pexels-photo-108941.jpeg"
+    click_on "Create Image"
+
+    expect(current_path).to eq(images_path)
+    expect(page).to have_content("Rainbow")
+    expect(page).to have_css("img[src*='pexels-photo-108941.jpeg']")
+  end
+end
