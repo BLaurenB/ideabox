a14a2de4b4bfff274f5310db35bfe446335f1e09
BLaurenB <blbillington1@gmail.com>
2017-10-26 09:23:03 -0600

    new user, login, and logout working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def application
-    @user = User.find([:id])
-  end 
+  # def application
+  #   @user = User.find([:id])
+  # end
 end
